9495e684be18cd8019b2077fe8a1f0c40a2b4946
Moulali Syed <syedmoulali1997@gmail.com>
2022-05-26 18:08:12 +0530

    PageObjectsBaseClassTestcases

---
+++ MyStoreProject/src/test/java/com/mystore/testcases/AddToCartPageTest.java
@@ -0,0 +1,40 @@
+package com.mystore.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.mystore.base.BaseClass;
+import com.mystore.pageobjects.AddToCartPage;
+import com.mystore.pageobjects.IndexPage;
+import com.mystore.pageobjects.SearchResultPage;
+
+public class AddToCartPageTest extends BaseClass {
+
+	IndexPage index;
+	SearchResultPage searchResultPage;
+	AddToCartPage addToCartPage;
+
+	@BeforeMethod
+	public void setup() {
+		launchApp();
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void addToCartTest() {
+		index = new IndexPage();
+		searchResultPage = index.searchProduct("t-shirt");
+		addToCartPage = searchResultPage.clickOnProduct();
+		addToCartPage.enterQuantity("2");
+		addToCartPage.selectSize("M");
+		addToCartPage.clickOnAddToCart();
+		boolean result = addToCartPage.validateAddToCart();
+		Assert.assertTrue(result);
+	}
+}
